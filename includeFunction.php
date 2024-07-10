0d6eb78233261f9583c1f8d909de23c420ffc341
mokad@Mokaddes_Ali <mokaddesali20040@gmail.com>
2024-07-10 11:28:23 +0600

    first commit

---
+++ includeFunction.php
@@ -0,0 +1,16 @@
+<!-- //The Php file included another php file 
+
+//The php file inclusion discuss two function
+//1. Include function and 2.Require Functions and -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include("menu.php"); ?>
+    <p>locale_get_display_language</p>
+</body>
+</html>
